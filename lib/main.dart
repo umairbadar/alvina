d82c826e2305f1aef70c1dc5fb271346b3425f9a
Syed Umair Badar <syedumairbadar@gmail.com>
2023-11-30 20:21:38 +0500

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,18 @@ import 'package:alvina/providers/product_listing_provider.dart';
 import 'package:alvina/providers/url_launcher_provider.dart';
 import 'package:alvina/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
+  load();
+}
+
+void load() async {
+  await dotenv.load(fileName: "assets/.env");
+  print(dotenv.get('FOO'));
 }
 
 class MyApp extends StatelessWidget {
